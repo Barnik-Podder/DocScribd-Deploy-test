69324e4cbe02d15b4aa0a37996f82b137de6885f
Barnik Podder <barnikpodder2003@gmail.com>
2025-02-28 08:53:28 +0530

    Update

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,27 @@
-const express = require('express');
-const app = express();
+const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const mongoDB = require("./configs/db");
-require('dotenv').config();
+require("dotenv").config();
 const signup_patient = require("./Routes/Signup_patient");
 const bookingRoutes = require("./Routes/BookingRoutes");
 
-
-
-
-const port = process.env.PORT||5001
-
+const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
 
 mongoDB();
 
-app.get('/', (req, res) => {
-  res.send("You are trying to access the backend over http");
-});
-
-app.use(express.json());
-
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
-
-app.use(cookieParser());
-
-app.use(express.json());
-
+// Define Routes
 app.use("/", signup_patient);
 app.use("/bookings", bookingRoutes);
+
+// Default Route
+app.get("/", (req, res) => {
+  res.send("You are trying to access the backend over HTTP");
+});
+
+// Export as a Serverless Function for Vercel
+module.exports = app;
